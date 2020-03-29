465ae405b648b918ce61816a33740025d0806921
chiarcos <christian.chiarcos@web.de>
2020-03-29 05:42:50 +0200

    simplified dimlex tables generated from lemon

---
+++ discourse-markers/tsv/README.md
@@ -0,0 +1,3 @@
+# TSV files with discourse markers per language
+
+- using CoNLL-RDF to process mapping files, to import lemon dimlexes and to create a table of written representations and PDTB relations
---
+++ discourse-markers/tsv/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# iterate over all mapping files, use CoNLL-RDF to create TSV files with written representations of discourse markers and their PDTB relation
+
+# system settings		UPDATE!
+##################
+
+CONLL_RDF=~/conll-rdf
+# set to your local CoNLL-RDF installation
+
+# conversion			DO NOT CHANGE!
+#############
+if ls $CONLL_RDF/run.sh | egrep '.' >&/dev/null; then # found
+
+	for dimlex in ../lemon/*/; do
+		if [ -e $dimlex/mapping.tsv ] ; then
+			for dict in $dimlex/*ttl; do
+				tgt=`echo $dict | sed -e s/'.*\/'// -e s/'\.ttl$'//`.tsv;
+				cat $dimlex/mapping.tsv | \
+				$CONLL_RDF/run.sh CoNLLStreamExtractor \
+					https://ignore.me/ \
+					SENSE PDTB3 | \
+				$CONLL_RDF/run.sh CoNLLRDFUpdater \
+					-custom -model $dict http://dimlex-lemon.org \
+					-updates mapping2tsv.sparql | \
+				$CONLL_RDF/run.sh CoNLLRDFFormatter -sparqltsv return-table.sparql > $tgt
+			done;
+		fi;
+	done;
+else
+	echo . 1>&2;
+	echo did not find CoNLL-RDF, get it from https://github.com/acoli-repo/conll-rdf and configure the local "$"CONLL_RDF variable 1>&2;
+fi;
