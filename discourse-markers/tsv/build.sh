f1b41d9b5aec834703c0a1ed9a5ba3315bf4e160
chiarcos <christian.chiarcos@web.de>
2020-03-31 00:06:16 +0200

    TED: Lithuanian, Turkish

--- discourse-markers/tsv/build.sh
+++ discourse-markers/tsv/build.sh
@@ -22,11 +22,11 @@ if ls $CONLL_RDF/run.sh | egrep '.' >&/dev/null; then # found
 					$CONLL_RDF/run.sh CoNLLStreamExtractor \
 						https://ignore.me/ \
 						SENSE PDTB3 | \
-						tee tmp.ttl | \
+						# tee tmp.ttl | \
 					$CONLL_RDF/run.sh CoNLLRDFUpdater \
 						-custom -model $dict http://dimlex-lemon.org \
 						-updates mapping2tsv.sparql | \
-						tee tmp2.ttl | \
+						# tee tmp2.ttl | \
 					$CONLL_RDF/run.sh CoNLLRDFFormatter -sparqltsv return-table.sparql > $tgt
 				fi;
 			done;
